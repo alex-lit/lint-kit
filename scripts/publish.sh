abd1403b52b279f65b9e75586c692149da686f1f
Alexey Litovchenko <alex.lit@outlook.com>
2020-12-28 16:14:47 +0300

    chore: fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,5 @@
 # Публикация пакета в npm репозитории
 # Предварительно нужно залогиниться - npm login
+# Если требует сделать перозиторий публичным - npm publish --access public
 
-npm publish --access public
+echo "publishing to npm..."
