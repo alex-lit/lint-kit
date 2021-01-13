0d5aa8a509fbd771c3a356f40c972811b4c8edbf
Alexey Litovchenko <alex.lit@outlook.com>
2021-01-13 12:37:29 +0300

    chore: update deps

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,4 +1,9 @@
-# Обновляет зависимости
-npx npm-check-updates -u
+# Обновляет зависимости (latest, newest, greatest, minor, patch)
+
+VERSION=${1:-minor}
+
+echo 'Обновление зависимостей до версии:' ${VERSION}
+
+npx npm-check-updates --upgrade --target ${VERSION}
 npm i
 npm audit fix
