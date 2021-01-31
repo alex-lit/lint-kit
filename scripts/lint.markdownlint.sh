7e3aa88cf1e4910037178a2425c5085a96432366
Alexey Litovchenko <alex.lit@outlook.com>
2021-01-31 17:32:13 +0300

    chore: preparing for version update

--- scripts/lint.markdownlint.sh
+++ scripts/lint.markdownlint.sh
@@ -7,7 +7,7 @@ COLOR_DEFAULT='\e[39m'
 
 FILES='**/*.md'
 
-echo "${COLOR_NAME}markdownlint + prettier ${COLOR_ARROW}-> ${COLOR_FILES}${FILES}${COLOR_DEFAULT}"
+echo "${COLOR_NAME}markdownlint ${COLOR_ARROW}-> ${COLOR_FILES}${FILES}${COLOR_DEFAULT}"
 
 prettier --write ${FILES}
 markdownlint ${FILES} --config .markdownlintrc.js --fix
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,13 +1,11 @@
 # Запускает общий линтер
 
-OPTION=${1} # example: --fix
-
 COLOR_TITLE='\e[92m'
-COLOR_LINTERS='\e[90m'
+COLOR_LINTERS='\e[33m'
 COLOR_DEFAULT='\e[39m'
 
 echo "${COLOR_TITLE}linters: ${COLOR_LINTERS}eslint, stylelint, markdownlint${COLOR_DEFAULT}"
 
-npm run lint:eslint ${OPTION}
-npm run lint:stylelint ${OPTION}
-npm run lint:markdownlint ${OPTION}
+npm run lint:eslint
+npm run lint:stylelint
+npm run lint:markdownlint
