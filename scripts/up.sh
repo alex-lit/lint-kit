2e9cb1bdd8bf3b16d35600bd9f82787ac4133a4a
Alexey Litovchenko <alex.lit@outlook.com>
2021-02-11 15:54:44 +0300

    chore: preparing for version update

--- scripts/up.sh
+++ scripts/up.sh
@@ -7,4 +7,4 @@ DEFAULT='\e[39m'
 echo "Upgrading dependencies to version: ${YELLOW}${VERSION}${DEFAULT}"
 
 npx npm-check-updates --upgrade --target ${VERSION}
-npm i --no-optional
+npm i --no-optional --legacy-peer-deps
