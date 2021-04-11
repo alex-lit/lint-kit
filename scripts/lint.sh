6c1e5bd7685d77b3d6732922dae30bf79adff2d5
Alexey Litovchenko <alex.lit@outlook.com>
2021-04-12 00:50:49 +0300

    chore: preparing for version update

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,8 +6,9 @@ COLOR_TITLE='\e[92m'
 COLOR_LINTERS='\e[33m'
 COLOR_DEFAULT='\e[39m'
 
-echo "${COLOR_TITLE}linters: ${COLOR_LINTERS}eslint, stylelint, markdownlint${COLOR_DEFAULT}"
+echo "${COLOR_TITLE}linters: ${COLOR_LINTERS}eslint, htmllint, stylelint, markdownlint${COLOR_DEFAULT}"
 
 npm run lint:eslint ${OPTION}
-npm run lint:stylelint ${OPTION}
+npm run lint:htmllint
 npm run lint:markdownlint ${OPTION}
+npm run lint:stylelint ${OPTION}
