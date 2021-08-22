c1f5e0e4f53971c6bebf3df13ad96809ba76aa95
Alexey Litovchenko <alex.lit@outlook.com>
2021-08-22 12:39:54 +0300

    refactor: use submodules

---
+++ scripts/postinstall.sh
@@ -0,0 +1,31 @@
+# Install submodules dependencies
+
+cd submodules
+
+cd config-commitlint
+npm i
+cd ../
+
+cd config-eslint
+npm i
+cd ../
+
+cd config-htmllint
+npm i
+cd ../
+
+cd config-markdownlint
+npm i
+cd ../
+
+cd config-npmlint
+npm i
+cd ../
+
+cd config-prettier
+npm i
+cd ../
+
+cd config-stylelint
+npm i
+cd ../
