2c469d9be34a7eac2cb70de30e65031c9558b973
Alexey Litovchenko <alex.lit@outlook.com>
2021-12-21 23:45:43 +0300

    ci: add up script

---
+++ scripts/ou.sh
@@ -0,0 +1,40 @@
+# Check outdated packages
+
+COLOR_NAME='\e[96m'
+
+cd submodules
+
+echo "\n${COLOR_NAME}check commitlint..."
+cd config-commitlint
+npm ou
+cd ../
+
+echo "\n${COLOR_NAME}check eslint..."
+cd config-eslint
+npm ou
+cd ../
+
+echo "\n${COLOR_NAME}check htmllint..."
+cd config-htmllint
+npm ou
+cd ../
+
+echo "\n${COLOR_NAME}check markdownlint..."
+cd config-markdownlint
+npm ou
+cd ../
+
+echo "\n${COLOR_NAME}check npmlint..."
+cd config-npmlint
+npm ou
+cd ../
+
+echo "\n${COLOR_NAME}check prettier..."
+cd config-prettier
+npm ou
+cd ../
+
+echo "\n${COLOR_NAME}check stylelint..."
+cd config-stylelint
+npm ou
+cd ../
