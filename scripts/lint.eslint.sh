d49ccce1676054556fa05c284ee382c140a2e9c4
Alexey Litovchenko <alex.lit@outlook.com>
2023-08-24 11:08:46 +0300

    chore: preparing for version update

--- scripts/lint.eslint.sh
+++ scripts/lint.eslint.sh
@@ -15,4 +15,4 @@ if [ $OPTION ] && [ $OPTION = 'fix' ]; then
   echo -e "${COLOR_ARROW}autofix:${COLOR_DEFAULT} true"
 fi
 
-eslint --ext ${FILES} --ignore-path .gitignore ./ --${OPTION}
+eslint --ext ${FILES} --ignore-path .gitignore --ignore-path .eslintignore ./ --${OPTION}
