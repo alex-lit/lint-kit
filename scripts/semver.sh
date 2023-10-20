0a92d3ab7c95e6d963d07e800aeea50f1acf8978
Alexey Litovchenko <alex.lit@outlook.com>
2023-10-20 11:37:41 +0300

    chore: preparing for version update

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -2,7 +2,8 @@
 
 git add --all
 git commit -m 'chore: preparing for version update'
+
 npm version $1
 
-# conventional-changelog -p angular -i CHANGELOG.md -s $1
-# git add CHANGELOG.md
+conventional-changelog -p angular -i CHANGELOG.md -s $1
+git add CHANGELOG.md
