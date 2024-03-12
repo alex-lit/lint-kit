24f1362a8fa486b3fec30e92966431a0f2c0b0cb
Alexey Litovchenko <alex.lit@outlook.com>
2024-03-12 15:40:14 +0300

    chore(@alexlit/config-stylelint): bump version

--- packages/config-stylelint/index.js
+++ packages/config-stylelint/index.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-console, import/extensions */
-
-import { config } from './config.js';
+/* eslint-disable no-console, import/extensions, sonarjs/no-duplicate-string, unicorn/no-null */
 
 const ADDITIONAL_PLUGINS = {
   /** @see [stylelint-a11y](https://github.com/double-great/stylelint-a11y) */
@@ -68,45 +66,165 @@ const PLUGINS = {
 /**
  * Create "extends" field
  *
- * @param {PLUGINS} incomingPlugins Plugins
+ * @param {PLUGINS} plugins Enabled/disabled plugins list
  */
-const connectPlugins = (incomingPlugins = {}) => {
-  const extendsPlugins = [];
+const createPluginsList = (plugins = {}) => {
+  const pluginsList = [];
 
-  Object.entries({ ...PLUGINS, ...incomingPlugins })?.forEach(
-    ([name, isActive]) => {
-      if (isActive) {
-        extendsPlugins.push(`@alexlit/config-stylelint/plugins/${name}`);
-      }
-    },
-  );
+  Object.entries({ ...PLUGINS, ...plugins })?.forEach(([name, isActive]) => {
+    if (isActive) {
+      pluginsList.push(`@alexlit/config-stylelint/plugins/${name}`);
+    }
+  });
 
-  return extendsPlugins;
+  return pluginsList;
 };
 
 /**
  * Create stylelint config
  *
- * @param {PLUGINS} incomingPlugins Plugins
- * @param {import('stylelint').Config} incomingConfig Stylelint config
+ * @param {PLUGINS} plugins Enabled/disabled plugins list
+ * @param {import('stylelint').Config} options Stylelint config
  */
-const createConfig = (incomingPlugins = {}, incomingConfig = {}) => ({
-  ...incomingConfig,
+const createConfig = (plugins = {}, options = {}) => ({
+  ...options,
 
   extends: [
     'stylelint-config-standard',
-    ...connectPlugins(incomingPlugins),
-    ...(incomingConfig.extends ?? []),
+
+    ...createPluginsList(plugins),
+
+    ...(options.extends ?? []),
   ],
 
   ignoreFiles: [
-    ...(config.ignoreFiles ?? []),
-    ...(incomingConfig.ignoreFiles ?? []),
+    '.*/**',
+    'build/**',
+    'dist/**',
+    'docs/**',
+    'node_modules/**',
+    'storybook-*/**',
+
+    ...(options.ignoreFiles ?? []),
   ],
 
-  plugins: [...(config.plugins ?? []), ...(incomingConfig.plugins ?? [])],
+  plugins: options.plugins ?? [],
 
-  rules: { ...config.rules, ...incomingConfig.rules },
+  rules: {
+    'alpha-value-notation': 'number',
+    'annotation-no-unknown': [true, { ignoreAnnotations: ['default'] }],
+    'at-rule-disallowed-list': ['debug'],
+
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignoreAtRules: ['else'],
+      },
+    ],
+
+    'at-rule-no-unknown': null,
+    'at-rule-no-vendor-prefix': true,
+    'block-no-empty': null,
+    'color-function-notation': 'modern',
+    'color-hex-length': 'short',
+    'color-no-hex': true,
+    'color-no-invalid-hex': true,
+
+    'custom-property-empty-line-before': [
+      'always',
+      {
+        except: ['after-comment', 'after-custom-property', 'first-nested'],
+      },
+    ],
+
+    'custom-property-pattern': '^_?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'declaration-block-no-redundant-longhand-properties': null,
+
+    'declaration-property-value-no-unknown': [
+      true,
+      {
+        ignoreProperties: {
+          '/.+/': /^(v-(bind|deep|global|slotted)|theme|view)|\$/,
+        },
+      },
+    ],
+
+    'font-family-name-quotes': 'always-unless-keyword',
+    'function-no-unknown': null, // delegate to scss/function-no-unknown
+    'function-url-quotes': 'always',
+    'function-url-scheme-disallowed-list': ['/^data/', 'ftp', '/^http/'],
+    'hue-degree-notation': 'angle',
+    'keyframes-name-pattern': '^[a-z]+(-[a-z]+)*$',
+
+    'max-nesting-depth': [
+      6,
+      {
+        ignoreAtRules: ['each', 'media', 'supports', 'include'],
+      },
+    ],
+
+    'media-feature-name-no-unknown': [
+      true,
+      { ignoreMediaFeatureNames: ['screen'] },
+    ],
+
+    'media-feature-name-no-vendor-prefix': true,
+    'no-descending-specificity': null,
+    'no-empty-source': null,
+    'no-unknown-animations': true,
+    'number-max-precision': 3,
+
+    'property-no-unknown': [
+      true,
+      { ignoreProperties: ['align-tracks', 'animation-timeline'] },
+    ],
+
+    'property-no-vendor-prefix': true,
+
+    'rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: [],
+      },
+    ],
+
+    'selector-class-pattern': [
+      '^[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$',
+      {
+        message:
+          'Selector should be written as BEM "block__element--modifier--value" (selector-class-pattern)',
+      },
+    ],
+
+    'selector-combinator-disallowed-list': ['>>>', '/deep/'],
+
+    'selector-max-compound-selectors': 6,
+    'selector-max-id': 0,
+    'selector-no-qualifying-type': null,
+    'selector-no-vendor-prefix': true,
+    'selector-pseudo-element-disallowed-list': ['shadow'],
+
+    'selector-pseudo-element-no-unknown': [
+      true,
+      { ignorePseudoElements: ['v-deep', 'file-selector-button'] },
+    ],
+
+    'selector-type-no-unknown': [true, { ignoreTypes: ['ymaps'] }],
+    'shorthand-property-no-redundant-values': true,
+    'string-no-newline': null,
+    'time-min-milliseconds': 16,
+
+    'value-keyword-case': [
+      'lower',
+      { camelCaseSvgKeywords: true, ignoreFunctions: ['v-bind'] },
+    ],
+
+    'value-no-vendor-prefix': true,
+
+    ...options.rules,
+  },
 });
 
 export { createConfig };
