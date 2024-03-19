7c1b8bff6c88a600ba54a19e51b271c35c59e251
Alexey Litovchenko <alex.lit@outlook.com>
2024-03-19 09:35:37 +0300

    chore: wip

---
+++ examples/php.php
@@ -0,0 +1,118 @@
+<?php namespace AlexLit\EarthNullSchoolMaps\Components;
+
+use Cms\Classes\ComponentBase;
+use System\Classes\ApplicationException;
+
+class Air extends ComponentBase
+{
+  public function componentDetails()
+  {
+    return [
+      'name' => 'Air',
+      'description' => 'World air map',
+    ];
+  }
+
+  public function defineProperties()
+  {
+    return [
+      'height' => [
+        'title' => 'Height',
+        'type' => 'dropdown',
+        'options' => [
+          'surface/level' => 'Surface',
+          'isobaric/1000hPa' => '1000hPa',
+          'isobaric/850hPa' => '850hPa',
+          'isobaric/700hPa' => '700hPa',
+          'isobaric/500hPa' => '500hPa',
+          'isobaric/250hPa' => '250hPa',
+          'isobaric/70hPa' => '70hPa',
+          'isobaric/10hPa' => '10hPa',
+        ],
+        'default' => 'surface/level',
+      ],
+      'overlay' => [
+        'title' => 'Overlay',
+        'type' => 'dropdown',
+        'options' => [
+          '' => 'Wind',
+          'overlay=temp' => 'Temperature',
+          'overlay=relative_humidity' => 'Relative Humidity',
+          'overlay=wind_power_density' => 'Instantaneous Wind Power Density',
+          'overlay=precip_3hr' => '3-hour Precipitation Accumulation',
+          'overlay=cape' =>
+            'Convective Available Potential Energy from Surface',
+          'overlay=total_precipitable_water' => 'Total Precipitable Water',
+          'overlay=total_cloud_water' => 'Total Cloud Water',
+          'overlay=mean_sea_level_pressure' => 'Mean Sea Level Pressure',
+          'overlay=misery_index' => 'Misery Index',
+          'overlay=off' => 'None',
+        ],
+        'default' => '',
+      ],
+      'projection' => [
+        'title' => 'Projection',
+        'type' => 'dropdown',
+        'options' => [
+          'atlantis' => 'Atlantis',
+          'azimuthal_equidistant' => 'Azimuthal Equidistant',
+          'conic_equidistant' => 'Conic Equidistant',
+          'equirectangular' => 'Equirectangular',
+          'orthographic' => 'Orthographic',
+          'patterson' => 'Patterson',
+          'stereographic' => 'Stereographic',
+          'waterman' => 'Waterman Butterfly',
+          'winkel3' => 'Winkel Tripel',
+        ],
+        'default' => 'orthographic',
+      ],
+      'mapWidth' => [
+        'title' => 'Map width',
+        'type' => 'string',
+        'default' => '100%',
+        'description' => 'Map width in pixels or percents',
+        'placeholder' => 'map width',
+        'validationPattern' => '^[0-9]*%?$',
+        'validationMessage' => 'Map width must be integer or percent!',
+      ],
+      'mapHeight' => [
+        'title' => 'Map height',
+        'type' => 'string',
+        'default' => '480',
+        'description' => 'Map height in pixels or percents',
+        'placeholder' => 'map width',
+        'validationPattern' => '^[0-9]*%?$',
+        'validationMessage' => 'Map height must be integer or percent!',
+      ],
+      'longitude' => [
+        'title' => 'Longitude',
+        'type' => 'string',
+        'default' => '',
+        'description' => 'Longitude (-360 - 360). Example: -214.68',
+        'placeholder' => 'longitude',
+        'validationPattern' => '^-?[0-9]{1,3}\.?[0-9]*$',
+        'validationMessage' => 'Longitude must be decimal or integer!',
+      ],
+      'latitude' => [
+        'title' => 'Latitude',
+        'type' => 'string',
+        'default' => '',
+        'description' => 'Latitude (-360 - 360). Example: 14.2',
+        'placeholder' => 'latitude',
+        'validationPattern' => '^-?[0-9]{1,3}\.?[0-9]*$',
+        'validationMessage' => 'Latitude must be decimal or integer!',
+      ],
+      'zoom' => [
+        'title' => 'Zoom',
+        'type' => 'string',
+        'default' => '400',
+        'description' => 'Zoom (0-3000)',
+        'placeholder' => 'zoom',
+        'validationPattern' => '^[0-9]{1,4}$',
+        'validationMessage' => 'Zoom must be integer!',
+      ],
+    ];
+  }
+}
+
+?>
