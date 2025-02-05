fcf87b98bcfd5a2757595518bf18d4714629b610
Alexey Litovchenko <alex.lit@outlook.com>
2025-02-05 16:09:03 +0300

    chore(@alexlit/config-eslint): bump version

--- packages/config-eslint/plugins/tailwindcss.js
+++ packages/config-eslint/plugins/tailwindcss.js
@@ -1,27 +1,27 @@
-import tailwind from 'eslint-plugin-tailwindcss';
+// import tailwind from 'eslint-plugin-tailwindcss';
 
 /** @see [eslint-plugin-tailwindcss](https://github.com/francoismassart/eslint-plugin-tailwindcss) */
 export const tailwindcss = [
-  ...tailwind.configs['flat/recommended'],
-
-  {
-    rules: {
-      'tailwindcss/no-custom-classname': 'off',
-      'vue/static-class-names-order': 'off',
-    },
-    settings: {
-      tailwindcss: {
-        callees: [
-          'classnames',
-          'clsx',
-          'cn',
-          'ctl',
-          'cva',
-          'tv',
-          'tw',
-          'twMerge',
-        ],
-      },
-    },
-  },
+  // TODO: включить после обновления плагина
+  // ...tailwind.configs['flat/recommended'],
+  // {
+  //   rules: {
+  //     'tailwindcss/no-custom-classname': 'off',
+  //     'vue/static-class-names-order': 'off',
+  //   },
+  //   settings: {
+  //     tailwindcss: {
+  //       callees: [
+  //         'classnames',
+  //         'clsx',
+  //         'cn',
+  //         'ctl',
+  //         'cva',
+  //         'tv',
+  //         'tw',
+  //         'twMerge',
+  //       ],
+  //     },
+  //   },
+  // },
 ];
