9fda26c05db5276edcc2c16cba6e813012053e6f
Alexey Litovchenko <alex.lit@outlook.com>
2025-02-06 13:10:24 +0300

    chore(@alexlit/config-eslint): bump version

--- packages/config-eslint/plugins/tailwindcss.js
+++ packages/config-eslint/plugins/tailwindcss.js
@@ -4,24 +4,24 @@
 export const tailwindcss = [
   // TODO: включить после обновления плагина
   // ...tailwind.configs['flat/recommended'],
-  // {
-  //   rules: {
-  //     'tailwindcss/no-custom-classname': 'off',
-  //     'vue/static-class-names-order': 'off',
-  //   },
-  //   settings: {
-  //     tailwindcss: {
-  //       callees: [
-  //         'classnames',
-  //         'clsx',
-  //         'cn',
-  //         'ctl',
-  //         'cva',
-  //         'tv',
-  //         'tw',
-  //         'twMerge',
-  //       ],
-  //     },
-  //   },
-  // },
+  {
+    rules: {
+      // 'tailwindcss/no-custom-classname': 'off',
+      'vue/static-class-names-order': 'off',
+    },
+    // settings: {
+    //   tailwindcss: {
+    //     callees: [
+    //       'classnames',
+    //       'clsx',
+    //       'cn',
+    //       'ctl',
+    //       'cva',
+    //       'tv',
+    //       'tw',
+    //       'twMerge',
+    //     ],
+    //   },
+    // },
+  },
 ];
