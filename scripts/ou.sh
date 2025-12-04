f4f0b28a960fd569ec50795348ea47980e4d929f
Alexey Litovchenko <alex.lit@outlook.com>
2025-12-04 10:32:04 +0300

    chore: wip

--- scripts/ou.sh
+++ scripts/ou.sh
@@ -4,29 +4,6 @@ source "$(dirname $0)"/../bash.config.sh
 
 echo -e "${YELLOW}Проверяю наличие устаревших пакетов...${WHITE}"
 
-# Инициализация переменных
-local_mode=false
-
-# Обработка флагов
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    --local | -l) local_mode=true ;;
-    *)
-      echo "Неизвестный флаг: $1"
-      exit 1
-      ;;
-  esac
-  shift
-done
-
-# Вывод результатов
-if $local_mode; then
-  echo -e "${GREY}Режим проверки: ${WHITE}локальный${WHITE}"
-  npm outdated
-else
-  echo -e "${GREY}Режим проверки: ${WHITE}глобальный${WHITE}"
-  npm outdated \
-    --recursive
-fi
+npm outdated
 
 SAY_GOODBYE
