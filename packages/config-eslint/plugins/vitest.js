d537d2de5ffd2a1488b0449a03c6ec874719e1c4
Alexey Litovchenko <alex.lit@outlook.com>
2026-02-17 14:06:45 +0300

    chore(@alexlit/config-eslint): bump version

--- packages/config-eslint/plugins/vitest.js
+++ packages/config-eslint/plugins/vitest.js
@@ -1,13 +1,13 @@
-import plugin from 'eslint-plugin-vitest';
+import plugin from '@vitest/eslint-plugin';
 import { defineConfig } from 'eslint/config';
 
 /** @see [eslint-plugin-vitest](https://github.com/veritem/eslint-plugin-vitest) */
 export const vitest = defineConfig([
   {
-    files: ['*.{test,spec}.{js,ts}'],
+    files: ['*.{test,spec}.{js,cjs,mjs,ts}'],
     plugins: { vitest: plugin },
     rules: {
-      ...plugin.configs.all.rules,
+      ...plugin.configs.recommended.rules,
 
       'vitest/no-conditional-in-test': 'off',
       'vitest/no-conditional-tests': 'off',
--- packages/config-eslint/presets/default.js
+++ packages/config-eslint/presets/default.js
@@ -8,10 +8,8 @@ import { prettierConfig } from '../plugins/prettier.js';
 import { regexp } from '../plugins/regexp.js';
 import { sonar } from '../plugins/sonar.js';
 import { stylistic } from '../plugins/stylistic.js';
-import { tanstackQuery } from '../plugins/tanstack-query.js';
 import { typescript } from '../plugins/typescript.js';
 import { unicorn } from '../plugins/unicorn.js';
-import { vitest } from '../plugins/vitest.js';
 import { vueAccessibility } from '../plugins/vue-accessibility.js';
 import { vueI18n } from '../plugins/vue-i18n.js';
 import { vue } from '../plugins/vue.js';
@@ -27,10 +25,8 @@ export const defaultPreset = defineConfig([
   ...regexp,
   ...sonar,
   ...stylistic,
-  ...tanstackQuery,
   ...typescript,
   ...unicorn,
-  ...vitest,
   ...vue,
   ...vueAccessibility,
   ...vueI18n,
