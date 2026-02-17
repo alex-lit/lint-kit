f1299319df913c8ecda302938f716e0c1421c036
Alexey Litovchenko <alex.lit@outlook.com>
2026-02-17 12:03:39 +0300

    chore(@alexlit/config-eslint): bump version

--- packages/config-eslint/plugins/tailwindcss.js
+++ packages/config-eslint/plugins/tailwindcss.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'eslint/config';
 
-// import tailwind from 'eslint-plugin-tailwindcss';
+// import plugin from 'eslint-plugin-tailwindcss';
 import { FILES } from '../presets/base.js';
 
 /** @see [eslint-plugin-tailwindcss](https://github.com/francoismassart/eslint-plugin-tailwindcss) */
 export const tailwindcss = defineConfig([
   // TODO: включить после обновления плагина
-  // ...tailwind.configs['flat/recommended'],
+  // ...plugin.configs['flat/recommended'],
   {
     files: FILES,
     rules: {
--- packages/config-eslint/plugins/tanstack-query.js
+++ packages/config-eslint/plugins/tanstack-query.js
@@ -1,7 +1,7 @@
-import plugin from '@tanstack/eslint-plugin-query';
+// import plugin from '@tanstack/eslint-plugin-query';
 import { defineConfig } from 'eslint/config';
 
 /** @see [@tanstack/query](https://tanstack.com/query/latest/docs/eslint/eslint-plugin-query) */
 export const tanstackQuery = defineConfig([
-  ...plugin.configs['flat/recommended'],
+  // ...plugin.configs['flat/recommended'], // TODO: ждет обновления плагина
 ]);
