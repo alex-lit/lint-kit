491749443c9c3fa7dc52c7a33770d0de552675c3
Alexey Litovchenko <alex.lit@outlook.com>
2026-02-18 15:29:35 +0300

    chore(@alexlit/config-eslint): bump version

--- packages/config-eslint/plugins/unocss.js
+++ packages/config-eslint/plugins/unocss.js
@@ -1,10 +1,12 @@
-import plugin from '@unocss/eslint-config/flat';
+// import plugin from '@unocss/eslint-config/flat';
 import { defineConfig } from 'eslint/config';
 
-import { FILES } from '../presets/base.js';
+// import { FILES } from '../presets/base.js';
+
+// TODO: update plugin
 
 /** @see [@unocss/eslint-config](https://unocss.dev/integrations/eslint) */
 export const unocss = defineConfig([
-  { files: FILES, ...plugin },
-  { files: FILES, rules: { 'unocss/order-attributify': 'off' } },
+  // { files: FILES, ...plugin },
+  // { files: FILES, rules: { 'unocss/order-attributify': 'off' } },
 ]);
