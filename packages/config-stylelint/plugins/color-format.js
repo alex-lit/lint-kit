db251a86655511b65048703f9f050d06e2553bc1
Alexey Litovchenko <alex.lit@outlook.com>
2026-03-18 13:21:44 +0300

    chore(@alexlit/config-stylelint): bump version

--- packages/config-stylelint/plugins/color-format.js
+++ packages/config-stylelint/plugins/color-format.js
@@ -4,6 +4,6 @@
  * @see [stylelint-color-format](https://github.com/filipekiss/stylelint-color-format)
  */
 export default {
-  plugins: ['stylelint-color-format'],
-  rules: { 'color-format/format': { format: 'rgb' } },
+  // plugins: ['stylelint-color-format'], //TODO: update plugin from ^1.1.0
+  // rules: { 'color-format/format': { format: 'rgb' } },
 };
