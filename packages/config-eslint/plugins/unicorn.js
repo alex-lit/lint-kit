be6501f81358ea5f16f8e7952fb2839565505d43
Alexey Litovchenko <alex.lit@outlook.com>
2026-06-19 15:13:45 +0300

    chore(@alexlit/config-eslint): bump version

--- packages/config-eslint/plugins/unicorn.js
+++ packages/config-eslint/plugins/unicorn.js
@@ -14,23 +14,7 @@ export const unicorn = defineConfig([
         'error',
         { checkArrowFunctions: false },
       ],
-      'unicorn/no-array-reduce': ['error', { allowSimpleOperations: true }],
-      'unicorn/no-asterisk-prefix-in-documentation-comments': 'off',
-      'unicorn/no-empty-file': 'off',
-      'unicorn/no-for-each': 'off',
-      'unicorn/no-this-outside-of-class': 'off', // used in setters/getters
-      'unicorn/no-top-level-side-effects': 'off', // Vue.js specific
-      'unicorn/no-unused-properties': 'off', // don't respect local keys
-      'unicorn/prefer-dom-node-html-methods': 'off', // Safari not supports setHTML()
-      'unicorn/prefer-export-from': ['error', { checkUsedVariables: true }],
-      'unicorn/prefer-https': 'off', // SVG need HTTP
-      'unicorn/prefer-import-meta-properties': 'warn',
-      'unicorn/prefer-iterator-concat': 'off', // ES2026 only
-      'unicorn/prefer-module': 'warn',
-      'unicorn/prefer-node-protocol': 'warn',
-      'unicorn/prefer-temporal': 'off', // ES2026 only
-      'unicorn/prefer-type-literal-last': 'off', // perfectionist/sort-intersection-types
-      'unicorn/prevent-abbreviations': [
+      'unicorn/name-replacements': [
         'warn',
         {
           allowList: {
@@ -58,6 +42,22 @@ export const unicorn = defineConfig([
           replacements: { index18n: { i18n: true } },
         },
       ],
+      'unicorn/no-array-reduce': ['error', { allowSimpleOperations: true }],
+      'unicorn/no-asterisk-prefix-in-documentation-comments': 'off',
+      'unicorn/no-empty-file': 'off',
+      'unicorn/no-for-each': 'off',
+      'unicorn/no-this-outside-of-class': 'off', // used in setters/getters
+      'unicorn/no-top-level-side-effects': 'off', // Vue.js specific
+      'unicorn/no-unused-properties': 'off', // don't respect local keys
+      'unicorn/prefer-dom-node-html-methods': 'off', // Safari not supports setHTML()
+      'unicorn/prefer-export-from': ['error', { checkUsedVariables: true }],
+      'unicorn/prefer-https': 'off', // SVG need HTTP
+      'unicorn/prefer-import-meta-properties': 'warn',
+      'unicorn/prefer-iterator-concat': 'off', // ES2026 only
+      'unicorn/prefer-module': 'warn',
+      'unicorn/prefer-node-protocol': 'warn',
+      'unicorn/prefer-temporal': 'off', // ES2026 only
+      'unicorn/prefer-type-literal-last': 'off', // perfectionist/sort-intersection-types
       'unicorn/relative-url-style': ['error', 'always'],
     },
   },
